3214e362226af318fe3e022c655f09c718a8bffb
NgTMDuc <ducntm23052002@gmail.com>
2023-07-20 13:43:59 +0700

    Change script file

--- scripts/AFLW-DET-TRAIN.sh
+++ scripts/AFLW-DET-TRAIN.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0,1 python ./main.py \
-	--train_lists ./datasets/AFLW_lists/train.GTB \
-	--eval_ilists ./datasets/AFLW_lists/test.GTB \
+CUDA_VISIBLE_DEVICES=0,1 python /content/cpm/main.py \
+	--train_lists /content/cpm/datasets/AFLW_lists/train.GTB \
+	--eval_ilists /content/cpm/datasets/AFLW_lists/test.GTB \
 	--num_pts 5 \
         --data_indicator AFLW-19 \
 	--save_path ./snapshots/AFLW-CPM-DET 
